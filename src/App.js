23ecd47a995408251aba3e24f0a455b9918601c8
INotHaveName <alu.magallanes@gmail.com>
2022-03-21 18:35:53 -0300

    proyecto terminado...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import ListaDeTareas from './componentes/ListaDeTareas.js';
 
 function App() {
   return (
-    <div className="aplicacion-tareas">
-     <div className='freecodecamp-logo-contenedor'>
-
-       <img src={freeCodeCampLogo} className='freecodecamp-logo'
-       alt='logo freecodecamp'
-       />
-      </div>
-      <div className='lista-tareas-principal'>
+    <div className='aplicacion-tareas'>
+      
+      <div className='tareas-lista-principal'>
         <h1>Mis Tareas</h1>
-        
+        <ListaDeTareas />
       </div>
     </div>
   );
